fe759baacec97cd9606d59e83bda15d328af2856
cglewis <clewis@iqt.org>
2016-07-07 09:37:39 -0700

    stub more structure change

--- vcontrol/rest/version.py
+++ vcontrol/rest/version.py
@@ -2,14 +2,17 @@ from helpers import get_allowed
 
 import web
 
-class IndexR:
+class VersionR:
     """
-    This endpoint is just a quick way to ensure that the vcontrol API is up and
-    running properly
+    This endpoint is for returning the current running version of vcontrol
     """
     allow_origin, rest_url = get_allowed.get_allowed()
     def GET(self):
         """ GET HTTP Request """
         web.header("Content-Type","text/plain")
         web.header('Access-Control-Allow-Origin', self.allow_origin)
-        return "vcontrol"
+        try:
+            with open('VERSION', 'r') as f: v = f.read().strip()
+        except:
+            with open('../VERSION', 'r') as f: v = f.read().strip()
+        return v
--- vcontrol/vcontrol.py
+++ vcontrol/vcontrol.py
@@ -10,8 +10,15 @@ import subprocess
 import sys
 import web
 
+# cli classes
 from cli.version import VersionC
+
+# rest classes
 from rest.index import IndexR
+from rest.version import VersionR
+
+# rest providers classes
+from rest.providers.add import AddProviderR
 
 def get_allowed():
     rest_url = ""
@@ -27,22 +34,6 @@ def get_allowed():
 
 allow_origin, rest_url = get_allowed()
 
-# This endpoint returns the version of vcontrol that is currently running this API.
-class version:
-    def GET(self):
-        web.header("Content-Type","text/plain")
-        # set allowed origins for api calls
-        try:
-            allow_origin = os.environ["ALLOW_ORIGIN"]
-        except:
-            allow_origin = ''
-        web.header('Access-Control-Allow-Origin', allow_origin)
-        try:
-            with open('VERSION', 'r') as f: v = f.read().strip()
-        except:
-            with open('../VERSION', 'r') as f: v = f.read().strip()
-        return v
-
 # This endpoint allows for getting the current capacity of a particular provider. Currently only supports VMWare and OpenStack.
 class w_capacity:
     def GET(self, provider):
@@ -54,55 +45,6 @@ class w_capacity:
         web.header('Access-Control-Allow-Origin', allow_origin)
         # TODO for openstack and vmware
         return 1
-
-'''
-This endpoint allows for a new provider such as openstack or vmware to be added.
-A Vent machine runs on a provider. Note that a provider can only be added from localhost
-of the machine running vcontrol unless the environment variable VENT_CONTROL_OPEN=true is set on the server.
-'''
-class w_add_provider:
-    def OPTIONS(self):
-        return self.POST()
-
-    def POST(self):
-        # set allowed origins for api calls
-        try:
-            allow_origin = os.environ["ALLOW_ORIGIN"]
-        except:
-            allow_origin = ''
-        web.header('Access-Control-Allow-Origin', allow_origin)
-        web.header('Access-Control-Allow-Headers', "Content-type")
-        open_d = os.environ.get('VENT_CONTROL_OPEN')
-        # TODO is this sufficient? probably not...
-        if web.ctx.env["HTTP_HOST"] == 'localhost:8080' or open_d == "true":
-            data = web.data()
-            payload = {}
-            try:
-                payload = ast.literal_eval(data)
-                if type(payload) != dict:
-                    payload = ast.literal_eval(json.loads(data))
-            except:
-                return "malformed json body"
-
-            try:
-                if os.path.isfile('providers.txt'):
-                    with open('providers.txt', 'r') as f:
-                        for line in f:
-                            if line.split(":")[0] == payload['name']:
-                                return "provider already exists"
-                # only get here if it didn't already return
-                with open('providers.txt', 'a') as f:
-                    if payload['provider'] == 'openstack' or payload['provider'] == 'vmwarevsphere':
-                        f.write(payload['name']+':'+payload['provider']+':'+str(payload['cpu'])+":"+str(payload['ram'])+":"+str(payload['disk'])+":"+str(payload['args'])+'\n')
-                    elif payload['provider'] == 'virtualbox':
-                        f.write(payload['name']+':'+payload['provider']+'\n')
-                    else:
-                        f.write(payload['name']+':'+payload['provider']+':'+str(payload['args'])+'\n')
-                return "successfully added provider"
-            except:
-                return "unable to add provider"
-        else:
-            return "must be done from the localhost running vcontrol daemon"
 
 class w_boot_machine:
     def get(self, machine):
@@ -732,7 +674,7 @@ def get_urls():
         '/swagger.yaml', 'swagger',
         '/v1', IndexR,
         '/v1/', IndexR,
-        '/v1/add_provider', 'w_add_provider',
+        '/v1/add_provider', AddProviderR,
         '/v1/remove_provider/(.+)', 'w_remove_provider',
         '/v1/capacity/(.+)', 'w_capacity',
         '/v1/create_machine', 'w_create_machine',
@@ -764,7 +706,7 @@ def get_urls():
         '/v1/get_template', 'w_get_template',
         '/v1/register_machine', 'w_register_machine',
         '/v1/deregister_machine/(.+)', 'w_deregister_machine',
-        '/v1/version', 'version'
+        '/v1/version', VersionR
     )
     return urls
 
