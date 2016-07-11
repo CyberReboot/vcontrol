dbe8ffa1d5f717b3a409df1b012a483e5a1151e3
cglewis <clewis@iqt.org>
2016-07-11 08:42:24 -0700

    break out tests

--- vcontrol/test_vcontrol.py
+++ vcontrol/test_vcontrol.py
@@ -5,11 +5,18 @@ import vcontrol
 import pytest
 import web
 
-def test_web():
+def start_web_app():
+    """ starts the web app in a TestApp for testing """
     vc_inst = vcontrol.VControl()
     urls = vc_inst.urls()
     app = web.application(urls, globals())
     testApp = TestApp(app.wsgifunc())
+    return testApp
+
+def test_index_r():
+    """ tests the restful endpoint: index """
+    # get web app
+    testApp = start_web_app()
 
     # test index
     r = testApp.get('/v1')
@@ -18,6 +25,11 @@ def test_web():
     r = testApp.get('/v1/')
     assert r.status == 200
     assert r.normal_body == "vcontrol"
+
+def test_version_r():
+    """ tests the restful endpoint: version """
+    # get web app
+    testApp = start_web_app()
 
     # test version
     r = testApp.get('/v1/version')
