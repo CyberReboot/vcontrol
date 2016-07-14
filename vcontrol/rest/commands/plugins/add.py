9ec182bcb0d5961113d40e9f2fb28ecc076a4466
Joecakes4u <joecakes4u@gmail.com>
2016-07-14 15:38:23 -0700

    implement add_plugins. Closes #20

--- vcontrol/rest/commands/plugins/add.py
+++ vcontrol/rest/commands/plugins/add.py
@@ -1,13 +1,49 @@
 from ...helpers import get_allowed
 
+import ast
+import json
 import web
+import os
+import subprocess
 
 class AddPluginCommandR:
     """
     This endpoint is for adding a new plugin repository on a Vent machine.
     """
+
     allow_origin, rest_url = get_allowed.get_allowed()
-    def GET(self, machine, url):
+    def OPTIONS(self):
+        return self.POST()
+
+    def POST(self):
         web.header('Access-Control-Allow-Origin', self.allow_origin)
+        data = web.data()
+        payload = {}
+        try:
+            payload = ast.literal_eval(data)
+            if type(payload) != dict:
+                payload = ast.literal_eval(json.loads(data))
+        except:
+            return "malformed json body"
+
+        # TODO add --engine-label(s) vent specific labels
+        engine_labels = "--engine-label vcontrol_managed=yes "
+        try:
+            print str(payload)
+            if "machine" in payload.keys():
+                if "url" in payload.keys():
+                    url = payload["url"]
+                    cmd = "/usr/local/bin/docker-machine ssh "+payload["machine"]+" \"python2.7 /data/plugin_parser.py add_plugins "+url+"\""
+                    output = subprocess.check_output(cmd, shell=True)
+                    if output == "":
+                        output = "successfully installed "+url
+                    else:
+                        output = "failed to install "+url+"\n"+output
+                else:
+                    output = "failed to install "+url+" -- no url specified"
+            else:
+                output = "failed to install "+url+" -- no machine specified"
+        except Exception as e:
+        	output = str(e)
         # TODO
-        return 1
+        return output
