cf54e577271c0b1193c3e4ad1a7427f51e03e6cd
cglewis <clewis@iqt.org>
2016-07-16 20:14:06 -0700

    closes #31

--- vcontrol/rest/commands/info.py
+++ vcontrol/rest/commands/info.py
@@ -1,12 +1,18 @@
 from ..helpers import get_allowed
 
+import subprocess
 import web
 
 class InfoCommandR:
     """
     This endpoint is for getting info about a Vent machine.
     """
+    allow_origin, rest_url = get_allowed.get_allowed()
     def GET(self, machine):
         web.header('Access-Control-Allow-Origin', self.allow_origin)
-        # TODO
-        return 1
+        out = ""
+        try:
+            out = subprocess.check_output("/usr/local/bin/docker-machine ssh "+machine+" \"/data/info_tools/get_info.sh all -v\"", shell=True)
+        except:
+            out = "unable to get info on "+machine
+        return str(out)
