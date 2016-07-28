77910e3c67a0dac5fd1d90a94a5e2cb8e49cde74
Joecakes4u <joecakes4u@gmail.com>
2016-07-28 09:27:55 -0700

    Password restriction

--- vcontrol/cli/providers/add.py
+++ vcontrol/cli/providers/add.py
@@ -5,6 +5,13 @@ import os
 import requests
 
 class AddProviderC:
+    def splitarg(string):
+        import re
+        p = re.compile(r'"[\w ]+"')
+        quoted_item = p.search(string).group()
+        newstring = p.sub('', string)
+        return newstring.split() + [quoted_item]
+
     def add(self, provider, args, daemon):
         """
         only privileged can add providers, which currently is only
@@ -16,15 +23,22 @@ class AddProviderC:
             # split and get the end and append
             api_v = daemon.split('/')[-1]
             daemon = 'http://localhost:8080/'+api_v
+        print args.args
         if provider == "virtualbox":
             payload = {'name': args.name, 'provider': provider}
-        elif provider == "vmwarevsphere" and not "password" in args.args:
-            raw_pw = getpass.getpass()
-            pw = base64.b64encode(raw_pw)
-            args.args = args.args+" --vmwarevsphere-password "+pw
-            payload = {'name': args.name, 'provider': provider, 'args': args.args}
+        elif provider == "vmwarevsphere":
+            if not "--vmwarevsphere-password" in args.args:
+                raw_pw = getpass.getpass()
+                pw = base64.b64encode(raw_pw)
+                args.args = args.args+" --vmwarevsphere-password "+pw
+                payload = {'name': args.name, 'provider': provider, 'args': args.args}
+            else:
+                raw_pw = args.args.split("-password ")[1]
+                pw = base64.b64encode(raw_pw)
+                args.args = (" ").join(args.args.split(" ")[:-1])+" "+pw
+                payload = {'name': args.name, 'provider': provider, 'args': args.args}
         else:
-            raw_pw = args.args.split("password ")[-1]
+            raw_pw = args.args.split("-password ")[1]
             pw = base64.b64encode(raw_pw)
             args.args = (" ").join(args.args.split(" ")[:-1])+" "+pw
             payload = {'name': args.name, 'provider': provider, 'args': args.args}
