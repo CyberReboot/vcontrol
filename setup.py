fbbbf75b778f1704706a04dbdec697c5b996d7f9
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-08-31 15:09:50 -0700

    added setup.py; added make install to install vcontrol to python path.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.7
+from distutils.core import setup
+
+setup(
+  name='vcontrol',
+  version='0.1.4',
+  author='Chiraag Prafullchandra',
+  author_email='cprafullchandra@iqt.org',
+  maintainer='Charlie Lewis',
+  maintainer_email='clewis@iqt.org',
+  description='Vent control layer for managing Vent instances',
+  long_description=open('README.md').read(),
+  url='https://github.com/CyberReboot/vcontrol',
+  packages=['vcontrol', 'vcontrol.cli', 'vcontrol.rest', 'vcontrol.cli.commands',
+            'vcontrol.cli.commands.plugins', 'vcontrol.cli.machines', 'vcontrol.cli.providers',
+            'vcontrol.rest.commands', 'vcontrol.rest.commands.plugins', 'vcontrol.rest.helpers',
+            'vcontrol.rest.machines', 'vcontrol.rest.providers'],
+  scripts=['bin/vcontrol'],
+  data_files=[('api', ['api/Dockerfile', 'api/index.html', 'api/swagger-ui.js']),
+              ('docs', ['docs/README.md', 'AUTHORS', 'CHANGELOG.md', 'CONTRIBUTING.md', 'Dockerfile',
+               'LICENSE', 'Makefile', 'MAINTAINERS', 'README.md', 'VERSION']),
+              ('vcontrol', ['vcontrol/requirements.txt', 'vcontrol/swagger.yaml',
+               'vcontrol/vmware-stats'])],
+  license='LICENSE',
+  classifiers=[
+    'Programming Language :: Python',
+    'Operating System :: POSIX :: Linux :: Boot2Docker',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Environment :: Other Environment'
+  ]
+)
