e84be48565ef836a88c4e762971ba13138ba2e9d
Joecakes4u <joecakes4u@gmail.com>
2016-09-01 11:03:10 -0700

    bump to v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name='vcontrol',
-  version='0.1.4',
+  version='0.2.0',
   author='Chiraag Prafullchandra',
   author_email='cprafullchandra@iqt.org',
   maintainer='Charlie Lewis',
