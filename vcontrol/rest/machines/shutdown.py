a633fd37a4d795e7b565254ef10aaa0f2ad77f31
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-27 00:08:27 +0300

    Put the web.header function in a try/except block

--- vcontrol/rest/machines/shutdown.py
+++ vcontrol/rest/machines/shutdown.py
@@ -3,13 +3,18 @@ from ..helpers import get_allowed
 import subprocess
 import web
 
+
 class ShutdownMachineR:
     """
     This endpoint is for shutting down a running machine.
     """
     allow_origin, rest_url = get_allowed.get_allowed()
+
     def GET(self, machine):
-        web.header('Access-Control-Allow-Origin', self.allow_origin)
+        try:
+            web.header('Access-Control-Allow-Origin', self.allow_origin)
+        except Exception as e: # no pragma
+            print(e.message)
         try:
             out = subprocess.check_output("/usr/local/bin/docker-machine stop "+machine, shell=True)
         except:
