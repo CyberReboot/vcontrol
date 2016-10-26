8aa9985bfb2dec68c39c2f017730e8f60148db9e
cglewis <clewis@iqt.org>
2016-10-26 15:02:39 -0700

    forgot &&

--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-py.test /vcontrol -v --cov=/vcontrol/vcontrol --cov-report term-missing && cp .coverage /local-vent/ chmod 777 /local-vent/.coverage
+py.test /vcontrol -v --cov=/vcontrol/vcontrol --cov-report term-missing && cp .coverage /local-vent/ && chmod 777 /local-vent/.coverage
