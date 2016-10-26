ce4ba8eb117329763363cdf7cc4cde28b42c482d
Sean S <sshadmand@iqt.org>
2016-10-25 18:21:21 -0700

    added integration test

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,4 +1,5 @@
 from vcontrol.rest.commands import info
+# from vcontrol.rest.providers import list_all
 
 import requests
 
@@ -8,3 +9,11 @@ def test_info_command_r():
     assert r.text == 'unable to get info on foo'
     c_inst = info.InfoCommandR()
     c_inst.GET("foo")
+
+
+
+def test_providers_list_all():
+    """ tests the ListProvidersR class """
+    r = requests.get('http://localhost:8080/v1/providers/list')
+    assert r.status_code == 200
+    assert r.json() == {}
